f0278d0f62cd76d541156211d387c2a2bf434db7
dhammarkeswaar <dhamarkeswar092006@gmail.com>
2026-02-28 22:54:45 +0530

    UC2

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,13 +1,18 @@
 /*
-*OOPSBannerApp UC1
+*OOPSBannerApp UC2
 *
 *@author Dhammar
-*@version 1.0 
+*@version 2.0 
 */
-
 
 public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("   *** " + " " + "     *** " + " " + "   ****** " + " " + "       ***** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + "   **     ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + " ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " ****** " + " " + "      ***** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "               ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "     **      ** ");
+        System.out.println("   *** " + " " + "     *** " + " " + "   ** " + " " + "        ***** ");
     }
 }
